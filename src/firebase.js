712626bfbe3fe6ef1fe17cbc5a48a5350cf27275
Mouhamadou Kane <papykane123@gmail.com>
2024-05-21 00:16:02 +0200

    initialize firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,7 @@ const admin = require('firebase-admin');
 const serviceAccount = require('../config/mspr-payetonkawa-58875-73430-firebase-adminsdk-6zken-50aba7c0a3.json'); // Téléchargez ce fichier depuis la console Firebase
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://mspr-payetonkawa-58875-73430.firebaseio.com' // Remplacez <PROJECT_ID> par votre ID de projet
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
