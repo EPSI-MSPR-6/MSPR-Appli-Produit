6212098a0ab77634046087c62994e6c3c00edc4b
Mouhamadou Kane <papykane123@gmail.com>
2024-05-20 21:55:34 +0200

    initialize firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./mspr-payetonkawa-58875-73430-firebase-adminsdk-6zken-50aba7c0a3.json'); // Téléchargez ce fichier depuis la console Firebase
+const serviceAccount = require('./config/mspr-payetonkawa-58875-73430-firebase-adminsdk-6zken-50aba7c0a3.json'); // Téléchargez ce fichier depuis la console Firebase
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
