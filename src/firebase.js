bc6729cde912fe6de62ce8fd29958d85c7c4a658
Crispus <caakpocrispus27@gmil.com>
2024-06-03 11:28:26 +0200

    initial

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
+require('dotenv').config();
 
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        projectId: "mspr-payetonkawa-58875-73430"
+        projectId: "mspr-payetonkawa-58875-73430",
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+
     })
 });
 
