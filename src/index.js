df9d3b2579f94a5df70c11880040c633e72ab34f
Kanesho1924 <avande764@gmail.com>
2024-06-10 22:50:26 +0200

    Modification Arboresence

--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
 const express = require('express')
-const db = require('./firebase.js'); // Importer le fichier firebase.js
+const db = require('./firebase.js');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
-require('dotenv').config();
+const routes = require('./routes/products.js');
 
-// console.log(db)
+require('dotenv').config();
 
 const app = express()
 const port = 8081
+
 app.use(bodyParser.json());
 app.use('/api', routes);
 
-app.use(express.json());
 
-app.post('/add', async (req, res) => {
-    try {
-        const docRef = await db.collection('collection-name').add(req.body);
-        res.status(200).send('Document added with ID: ' + docRef.id);
-    } catch (error) {
-        res.status(500).send('Error adding document: ' + error.message);
-    }
-});
-
-// Ajoutez d'autres routes et logiques ici
 
 
 app.listen(port, () => {
-    console.log(`Service Produits en écoute sur le port ${port}`);
+    console.log(`API Produits en écoute sur le port ${port}`);
 });
